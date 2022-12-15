ea7747c4c150216f32f5943f7d512ed4a64e4ab1
KelliBre <Kelli.Brecunier@gmail.com>
2022-12-15 16:57:32 +1000

    commit tree c7ddce99486b59fb58b70d2da53a6e39065075ed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// GLOBAL SCOPE
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+//This is bad, do not do this. Tis a mere example of how this can be a problem.
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer ='Karen';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Caren"
+    return leastFavoriteCustomer;
+}
